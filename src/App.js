f0e2ae8eb574fcb4191029865e5c4a83ed385888
Felipe Lopes <felipee.c.lopess@gmail.com>
2019-10-17 15:36:34 -0300

    Ajustes capas dos filmes

--- src/App.js
+++ src/App.js
@@ -16,16 +16,25 @@ function App() {
       <body className ="main-body">
 
         <div className="cards">
-          {/* <Card linkImagem="https://cdn.pixabay.com/photo/2012/06/18/01/07/iguana-50198_960_720.jpg" nomeCard="Iguana"/> */}
+          <Card linkImagem="https://upload.wikimedia.org/wikipedia/pt/thumb/f/f6/P%C3%B4ster_Star_Wars.jpg/250px-P%C3%B4ster_Star_Wars.jpg" nomeCard="A new hope"/>
         </div>
         <div className="cards">
-          {/* <Card linkImagem="https://cdn.pixabay.com/photo/2016/02/07/18/19/brazil-1185279__340.jpg" nomeCard="Tucano"/> */}
+          <Card linkImagem="https://upload.wikimedia.org/wikipedia/pt/thumb/6/63/Star_Wars_The_Clone_Wars.jpg/250px-Star_Wars_The_Clone_Wars.jpg" nomeCard="Atack of clones"/>
         </div>
         <div className="cards">
-          {/* <Card linkImagem="https://cdn.pixabay.com/photo/2012/06/18/01/29/parrot-50202__340.jpg" nomeCard="Arara"/> */}
+          <Card linkImagem="https://upload.wikimedia.org/wikipedia/pt/thumb/0/05/Star_Wars_Phantom_Menace_-_P%C3%B4ster.jpg/250px-Star_Wars_Phantom_Menace_-_P%C3%B4ster.jpg" nomeCard="The phantom menace"/>
         </div>
         <div className="cards">
-          {/* <Card linkImagem="https://cdn.pixabay.com/photo/2017/06/09/09/39/adler-2386314__340.jpg" nomeCard="Aguia"/> */}
+          <Card linkImagem="https://upload.wikimedia.org/wikipedia/pt/thumb/5/58/Star_Wars_Epis%C3%B3dio_III_A_Vingan%C3%A7a_dos_Sith.jpg/250px-Star_Wars_Epis%C3%B3dio_III_A_Vingan%C3%A7a_dos_Sith.jpg" nomeCard="The revenge os the Sith"/>
+        </div>
+        <div className="cards">
+          <Card linkImagem="https://upload.wikimedia.org/wikipedia/pt/thumb/b/b2/ReturnOfTheJediPoster1983.jpg/250px-ReturnOfTheJediPoster1983.jpg" nomeCard="The return of the jedi"/>
+        </div>
+        <div className="cards">
+          <Card linkImagem="https://upload.wikimedia.org/wikipedia/pt/thumb/5/5c/The_Empire_Strikes_Back.jpg/250px-The_Empire_Strikes_Back.jpg" nomeCard="The empire is back"/>
+        </div>
+        <div className="cards">
+          <Card linkImagem="https://upload.wikimedia.org/wikipedia/pt/thumb/a/ae/Starwars_06.jpg/250px-Starwars_06.jpg" nomeCard="The Force Awakens"/>
         </div>
 
       </body>
--- src/card.js
+++ src/card.js
@@ -7,7 +7,7 @@ export default class Card extends React.Component{
     render(){
         return(
         <div className="card" style={{width: '18rem'}}>
-            <img src={this.props.linkImagem} className="card-img-top" alt="..." />
+            <img class="imagens-card" src={this.props.linkImagem} className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title">{this.props.nomeCard}</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>

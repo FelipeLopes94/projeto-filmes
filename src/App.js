310bc937b1163111c77df384d529a85d9768fd3a
Felipe Lopes <felipee.c.lopess@gmail.com>
2019-10-17 13:52:59 -0300

    mais ajustes nas props

--- src/App.js
+++ src/App.js
@@ -14,16 +14,16 @@ function App() {
       <body className ="main-body">
 
         <div className="cards">
-          <Card linkImagem="https://cdn.pixabay.com/photo/2012/06/18/01/07/iguana-50198_960_720.jpg"/>
+          <Card linkImagem="https://cdn.pixabay.com/photo/2012/06/18/01/07/iguana-50198_960_720.jpg" nomeCard="Iguana"/>
         </div>
         <div className="cards">
-          <Card linkImagem="https://cdn.pixabay.com/photo/2016/02/07/18/19/brazil-1185279__340.jpg"/>
+          <Card linkImagem="https://cdn.pixabay.com/photo/2016/02/07/18/19/brazil-1185279__340.jpg" nomeCard="Tucano"/>
         </div>
         <div className="cards">
-          <Card linkImagem="https://cdn.pixabay.com/photo/2012/06/18/01/29/parrot-50202__340.jpg"/>
+          <Card linkImagem="https://cdn.pixabay.com/photo/2012/06/18/01/29/parrot-50202__340.jpg" nomeCard="Arara"/>
         </div>
         <div className="cards">
-          <Card linkImagem="https://cdn.pixabay.com/photo/2017/06/09/09/39/adler-2386314__340.jpg"/>
+          <Card linkImagem="https://cdn.pixabay.com/photo/2017/06/09/09/39/adler-2386314__340.jpg" nomeCard="Aguia"/>
         </div>
 
       </body>
--- src/card.js
+++ src/card.js
@@ -9,7 +9,7 @@ export default class Card extends React.Component{
         <div className="card" style={{width: '18rem'}}>
             <img src={this.props.linkImagem} className="card-img-top" alt="..." />
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">{this.props.nomeCard}</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
             <Button type="submit">Submit</Button>

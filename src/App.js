dc105313c89e21883d29cd1d043759fb4b4ceb46
FelipeLopes94 <felipee.c.lopess@gmail.com>
2019-10-17 21:40:36 -0300

    Ajustes

--- src/App.js
+++ src/App.js
@@ -16,29 +16,48 @@ function App() {
       </header>
 
       <body className ="main-body">
+        
+        <div className="cards">
+          <Card linkImagem="https://upload.wikimedia.org/wikipedia/pt/thumb/f/f6/P%C3%B4ster_Star_Wars.jpg/250px-P%C3%B4ster_Star_Wars.jpg" nomeCard="A new hope">
+            <Modal/>
+          </Card> 
+        </div>
 
         <div className="cards">
-          <Card linkImagem="https://upload.wikimedia.org/wikipedia/pt/thumb/f/f6/P%C3%B4ster_Star_Wars.jpg/250px-P%C3%B4ster_Star_Wars.jpg" nomeCard="A new hope"/>
+          <Card linkImagem="https://upload.wikimedia.org/wikipedia/pt/thumb/6/63/Star_Wars_The_Clone_Wars.jpg/250px-Star_Wars_The_Clone_Wars.jpg" nomeCard="Atack of clones">
+            <Modal/>
+          </Card>  
         </div>
-        <div className="cards">
-          <Card linkImagem="https://upload.wikimedia.org/wikipedia/pt/thumb/6/63/Star_Wars_The_Clone_Wars.jpg/250px-Star_Wars_The_Clone_Wars.jpg" nomeCard="Atack of clones"/>
-        </div>
+
         <div className="cards">
           <Card linkImagem="https://upload.wikimedia.org/wikipedia/pt/thumb/0/05/Star_Wars_Phantom_Menace_-_P%C3%B4ster.jpg/250px-Star_Wars_Phantom_Menace_-_P%C3%B4ster.jpg" nomeCard="The phantom menace"/>
+            <Modal/>
         </div>
+
         <div className="cards">
-          <Card linkImagem="https://upload.wikimedia.org/wikipedia/pt/thumb/5/58/Star_Wars_Epis%C3%B3dio_III_A_Vingan%C3%A7a_dos_Sith.jpg/250px-Star_Wars_Epis%C3%B3dio_III_A_Vingan%C3%A7a_dos_Sith.jpg" nomeCard="The revenge os the Sith"/>
+          <Card linkImagem="https://upload.wikimedia.org/wikipedia/pt/thumb/5/58/Star_Wars_Epis%C3%B3dio_III_A_Vingan%C3%A7a_dos_Sith.jpg/250px-Star_Wars_Epis%C3%B3dio_III_A_Vingan%C3%A7a_dos_Sith.jpg" nomeCard="The revenge os the Sith">
+            <Modal/>
+          </Card>  
         </div>
+
         <div className="cards">
-          <Card linkImagem="https://upload.wikimedia.org/wikipedia/pt/thumb/b/b2/ReturnOfTheJediPoster1983.jpg/250px-ReturnOfTheJediPoster1983.jpg" nomeCard="The return of the jedi"/>
+          <Card linkImagem="https://upload.wikimedia.org/wikipedia/pt/thumb/b/b2/ReturnOfTheJediPoster1983.jpg/250px-ReturnOfTheJediPoster1983.jpg" nomeCard="The return of the jedi">
+            <Modal/>
+          </Card>   
         </div>
+
         <div className="cards">
-          <Card linkImagem="https://upload.wikimedia.org/wikipedia/pt/thumb/5/5c/The_Empire_Strikes_Back.jpg/250px-The_Empire_Strikes_Back.jpg" nomeCard="The empire is back"/>
+          <Card linkImagem="https://upload.wikimedia.org/wikipedia/pt/thumb/5/5c/The_Empire_Strikes_Back.jpg/250px-The_Empire_Strikes_Back.jpg" nomeCard="The empire is back">
+            <Modal/>
+          </Card> 
         </div>
+        
         <div className="cards">
-          <Card linkImagem="https://upload.wikimedia.org/wikipedia/pt/thumb/a/ae/Starwars_06.jpg/250px-Starwars_06.jpg" nomeCard="The Force Awakens" resumoFilme="teste"/>
+          <Card linkImagem="https://upload.wikimedia.org/wikipedia/pt/thumb/a/ae/Starwars_06.jpg/250px-Starwars_06.jpg" nomeCard="The Force Awakens" resumoFilme="teste">
+            <Modal/>
+          </Card> 
         </div>
-          <Modal/>
+         
       </body>
 
       <footer className="footer">
--- src/Card.js
+++ src/Card.js
@@ -2,19 +2,17 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from './Modal';
 
-export const infos = "Batata";
 
-export default class Card extends React.Component{
-    render(){
-        return(
+export default props =>
+        
+    <div>
         <div className="card" style={{width: '18rem'}}>
-            <img class="imagens-card" src={this.props.linkImagem} className="card-img-top" alt="..." />
+            <img class="imagens-card" src={props.linkImagem} className="card-img-top" alt="..." />
             <div className="card-body">
-                <h5 className="card-title">{this.props.nomeCard}</h5>
+                <h5 className="card-title">{props.nomeCard}</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
             <Button type="submit" data-toggle="modal" data-target="#ExemploModalCentralizado">Resumo do Filme</Button>
         </div>
-        )
-    }
-}
+    </div>
+

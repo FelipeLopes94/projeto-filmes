c6a6ecd2bbec3aa4c74249d610d50c646e5ae883
Felipe Lopes <felipee.c.lopess@gmail.com>
2019-10-18 16:57:43 -0300

    novos ajustes.map

--- src/components/Card.js
+++ src/components/Card.js
@@ -15,34 +15,35 @@ class Card extends React.Component {
   state = {};
   render() {
     if (this.state.filmes === undefined) return <p>carregando</p>;
-    let m = "";
 
     return (
       <div>
-        {this.state.filmes.map(filme => filme.title)}
-
-        <div className="card" style={{ width: "18rem" }}>
-          <img
-            class="imagens-card"
-            src={this.props.linkImagem}
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">{this.state.filmes[0].title}</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-          </div>
-          <Button
-            type="submit"
-            data-toggle="modal"
-            data-target="#ExemploModalCentralizado"
-          >
-            Resumo do Filme
-          </Button>
-        </div>
+        {this.state.filmes.map(filme => {
+          return (
+            <div className="card" style={{ width: "18rem" }}>
+              <img
+                class="imagens-card"
+                src={this.props.linkImagem}
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">{filme.title}</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+              <Button
+                type="submit"
+                data-toggle="modal"
+                data-target="#ExemploModalCentralizado"
+              >
+                Resumo do Filme
+              </Button>
+            </div>
+          );
+        })}
       </div>
     );
   }

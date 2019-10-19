7c9f149d138419cfe32ed07dbe0f6df413a6bef9
FelipeLopes94 <felipee.c.lopess@gmail.com>
2019-10-19 14:37:01 -0300

    NPM RUN BUILD

---
+++ build/precache-manifest.b0a310d39481d3b922017f4bf4a1e422.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a73aa3b5531dd27e5ba1ad8539a4f8",
+    "url": "/FelipeLopes94/projeto-filmes/index.html"
+  },
+  {
+    "revision": "0e4a05f00caeab45b958",
+    "url": "/FelipeLopes94/projeto-filmes/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "f4e70291b2f88ce0f713",
+    "url": "/FelipeLopes94/projeto-filmes/static/css/main.abe781b7.chunk.css"
+  },
+  {
+    "revision": "0e4a05f00caeab45b958",
+    "url": "/FelipeLopes94/projeto-filmes/static/js/2.a7649c0b.chunk.js"
+  },
+  {
+    "revision": "f4e70291b2f88ce0f713",
+    "url": "/FelipeLopes94/projeto-filmes/static/js/main.a983b316.chunk.js"
+  },
+  {
+    "revision": "5177fc5b4dbe69ea9830",
+    "url": "/FelipeLopes94/projeto-filmes/static/js/runtime-main.86fb0cb8.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/FelipeLopes94/projeto-filmes/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "e93baa5b4c5adb0df588cc4c0b0c1906",
+    "url": "/FelipeLopes94/projeto-filmes/static/media/fundo-star-wars.e93baa5b.jpg"
+  }
+]);
